cb345f71aee20edba550e0b5bcb1dbda40847d8b
Carl Avidano <cavidano@gmail.com>
2024-11-10 06:54:24 -0500

    adding newsletter

--- natura11y/webpack.prod.js
+++ natura11y/webpack.prod.js
@@ -1,17 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const path = require('path');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); // For JS minification
 
 module.exports = merge(common, {
     mode: 'production',
     target: 'browserslist',
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
         iife: true
     },
     module: {
@@ -36,21 +33,34 @@ module.exports = merge(common, {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
+                    MiniCssExtractPlugin.loader,  // Extracts CSS for production
                     'css-loader',
-                    { 
-                        loader: 'postcss-loader'
-                    },
+                    'postcss-loader',
                     'sass-loader',
                 ],
             },
         ],
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,  // Remove comments
+                    },
+                },
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin() // Minifies CSS
+        ],
+        splitChunks: {
+            chunks: 'all', // Code splitting for JS to improve caching
+        },
+    },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: 'dist/css/[name].css', // Outputs CSS to web/css
         }),
         new CssMinimizerPlugin()
     ]
